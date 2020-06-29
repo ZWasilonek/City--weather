3386f85289ef6d82c71a2a63e13a7bbb2a13ec38
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-29 18:40:17 +0200

    open-weather.js method findByCity added

--- weather_app/js/open-weather.js
+++ weather_app/js/open-weather.js
@@ -1,6 +1,5 @@
 import {OW_API_KEY} from '../../api_keys.js';
 
-// const openWeatherApi =`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${OW_API_KEY}`
 const herokuCORS = 'https://cors-anywhere.herokuapp.com/';
 
 class OpenWeatherMap {};
@@ -31,34 +30,25 @@ OpenWeatherMap.prototype.getCurrentWeatherByGeoCoordinates = function(latitude, 
 										.then((data, err)=>sendResponse(data, err, callback));
 };
 
-
-
-
-OpenWeatherMap.prototype.getThreeHourForecastByCityName = function(cityName, callback) {
-	const requestURL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${OW_API_KEY}&units=metric&lang=pl`;
-	return fetch(herokuCORS+requestURL).then(resp=>resp.json())
-	  								.then((data, err)=>sendResponse(data, err, callback));
+OpenWeatherMap.prototype.getWeatherByTime = function(latitude, longitude, time, callback) {
+	const requestURL = `api.openweathermap.org/data/2.5/onecall/timemachine?lat=${parseFloat(latitude)}&lon=${parseFloat(longitude)}
+	&dt=${time}&appid=${OW_API_KEY}&units=metric&lang=pl`;
+  return fetch(herokuCORS+requestURL).then(resp=>resp.json())
+										.then((data, err)=>sendResponse(data, err, callback));
 };
 
-
-OpenWeatherMap.prototype.getThreeHourForecastByGeoCoordinates = function(latitude, longitude, callback) {
-	const requestURL = `api.openweathermap.org/data/2.5/forecast?lat=${parseFloat(latitude)}&lon=${parseFloat(longitude)}&appid=${OW_API_KEY}&units=metric&lang=pl`;
+OpenWeatherMap.prototype.getWeekForecastByGeoCoordinates = function(latitude, longitude, callback) {
+	const requestURL = `api.openweathermap.org/data/2.5/forecast?lat=${parseFloat(latitude)}&lon=${parseFloat(longitude)}&
+	exclude=hourly,daily&appid=${OW_API_KEY}&units=metric&lang=pl`;
 	return fetch(herokuCORS+requestURL).then(resp=>resp.json())
 										.then((data, err)=>sendResponse(data, err, callback));
 };
 
-// OpenWeatherMap.prototype.getCurrentWeatherByIP = function (lat, callback) {
-// 	const weatherByIp = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.api_key}`
-// }
-
-// let weather = new OpenWeatherMap();
-// weather.getCurrentWeatherByCityName('Warszawa',(currentWeather, err)=>{
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log('currentWeather', currentWeather);
-//     }
-// })
+OpenWeatherMap.prototype.getWeekForecastByGeoCoordinates = function(cityName, callback) {
+	const requestURL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&
+	exclude=hourly,daily&appid=${OW_API_KEY}&units=metric&lang=pl`;
+	return fetch(herokuCORS+requestURL).then(resp=>resp.json())
+										.then((data, err)=>sendResponse(data, err, callback));
+};
 
 export {OpenWeatherMap};
