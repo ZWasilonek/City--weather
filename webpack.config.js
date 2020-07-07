1e19285639e9ae6430c0e9035088bb8bb50a4b12
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-07-07 14:39:11 +0200

    webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const PORT = process.env.PORT || 5000;
+const entryPath = "weather_app";
+const entryFile = "app.js";
+const path = require("path");
+const Compression = require("compression-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: ["whatwg-fetch", `./${entryPath}/js/${entryFile}`],
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, `${entryPath}/build`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `${entryPath}`),
+    publicPath: "/build/",
+    compress: true,
+    port: 5000
+  },
+  mode: "development",
+  devtool: "source-map",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          publicPath: "/images/icons",
+          outputPath: "/images/icons"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new Compression({
+      threshold: 0,
+      minRatio: 0.8
+    }),
+    new Dotenv({
+      path: './.env',
+      safe: true 
+    })
+  ],
+}
+
